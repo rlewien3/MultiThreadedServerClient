98760946833aba745374dfaba3a1623c8881e475
rlewien3 <rlewien3@gmail.com>
2020-04-19 15:08:40 +1000

    Pretty and functional :)

--- src/client/ClientView.java
+++ src/client/ClientView.java
@@ -29,9 +29,11 @@ import java.awt.Dimension;
 
 public class ClientView implements Runnable {
 
-	private final String DEFAULT_TOAST = "Ryan's Dictionary";
+	private final String DEFAULT_TOAST = "Pocket Dictionary";
 	
 	private JFrame frame;
+	
+	private JPanel searchPanel;
 	private JTextField queryField;
 	private JTextPane textPane;
 	private JScrollPane scrollPane;
@@ -75,8 +77,10 @@ public class ClientView implements Runnable {
 			e.printStackTrace();
 		}
 		
+		// Total frame of program
 		frame = new JFrame();
-		frame.setBounds(100, 100, 440, 682);
+		frame.setTitle(DEFAULT_TOAST);
+		frame.setBounds(100, 100, 365, 380);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
      	gridBagLayout.columnWidths = new int[]{497, 0};
@@ -85,31 +89,34 @@ public class ClientView implements Runnable {
      	gridBagLayout.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
      	frame.getContentPane().setLayout(gridBagLayout);
 		
-		/** Search Panel **/
-        JPanel searchPanel = new JPanel();
+     	
+		/** Search Tab **/
+        searchPanel = new JPanel();
         GridBagLayout gbl_searchPanel = new GridBagLayout();
-        gbl_searchPanel.columnWidths = new int[]{20, 203, 0, 20, 0};
-        gbl_searchPanel.rowHeights = new int[]{20, 0, 0, 0, 150, 30, 20, 0};
-        gbl_searchPanel.columnWeights = new double[]{1.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-        gbl_searchPanel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 20.0, 1.0, 1.0, Double.MIN_VALUE};
+        gbl_searchPanel.columnWidths = new int[]{20, 203, 0, 15, 0};
+        gbl_searchPanel.rowHeights = new int[]{25, 17, 0, 10, 150, 20};
+        gbl_searchPanel.columnWeights = new double[]{1.0, 5.0, 0.0, 1.0, Double.MIN_VALUE};
+        gbl_searchPanel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 20.0, 1.0};
         searchPanel.setLayout(gbl_searchPanel);
         
         // Search title
-        JLabel lblNewLabel_3 = new JLabel("What cha lookin for?");
-        lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 14));
-        GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-        gbc_lblNewLabel_3.anchor = GridBagConstraints.WEST;
-        gbc_lblNewLabel_3.gridwidth = 2;
-        gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_3.gridx = 1;
-        gbc_lblNewLabel_3.gridy = 1;
-        searchPanel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+        JLabel searchTitle = new JLabel("What're you lookin for?");
+        searchTitle.setFont(new Font("Tahoma", Font.BOLD, 14));
+        GridBagConstraints gbc_searchTitle = new GridBagConstraints();
+        gbc_searchTitle.anchor = GridBagConstraints.WEST;
+        gbc_searchTitle.gridwidth = 2;
+        gbc_searchTitle.gridheight = 1;
+        gbc_searchTitle.insets = new Insets(0, 0, 5, 5);
+        gbc_searchTitle.gridx = 1;
+        gbc_searchTitle.gridy = 1;
+        searchPanel.add(searchTitle, gbc_searchTitle);
         
         // Search query field
         queryField = new JTextField();
         GridBagConstraints gbc_queryField = new GridBagConstraints();
-        gbc_queryField.fill = GridBagConstraints.BOTH;
-        gbc_queryField.insets = new Insets(0, 0, 5, 5);
+        gbc_queryField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_queryField.gridheight = 1;
+        gbc_queryField.insets = new Insets(0, 0, 0, 5);
         gbc_queryField.gridx = 1;
         gbc_queryField.gridy = 2;
         searchPanel.add(queryField, gbc_queryField);
@@ -117,8 +124,9 @@ public class ClientView implements Runnable {
         // Search query submit button
         JButton queryButton = new JButton("Search");
         GridBagConstraints gbc_queryButton = new GridBagConstraints();
-        gbc_queryButton.fill = GridBagConstraints.VERTICAL;
-        gbc_queryButton.insets = new Insets(0, 0, 5, 5);
+        gbc_queryButton.fill = GridBagConstraints.HORIZONTAL;
+        gbc_queryButton.gridheight = 1;
+        gbc_queryButton.insets = new Insets(0, 0, 0, 5);
         gbc_queryButton.gridx = 2;
         gbc_queryButton.gridy = 2;
         searchPanel.add(queryButton, gbc_queryButton);
@@ -130,14 +138,9 @@ public class ClientView implements Runnable {
         	}
         });
         
-        
+        // Search results text pane
         textPane = new JTextPane();
-        
-
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.anchor = GridBagConstraints.WEST;
-        constraints.fill = GridBagConstraints.VERTICAL;
-        constraints.gridy = 1;
+        textPane.setEditable(false);
         
         scrollPane = new JScrollPane();
         scrollPane.setBorder(new EmptyBorder(0,0,0,0));
@@ -148,38 +151,41 @@ public class ClientView implements Runnable {
         gbc_scrollPane.gridx = 1;
         gbc_scrollPane.gridy = 4;
         scrollPane.setViewportView(textPane);
-        
         searchPanel.add(scrollPane, gbc_scrollPane);
 
         
-        /** Edit Panel **/
+        
+        
+        /** Edit Tab **/
         JPanel editPanel = new JPanel();
         GridBagLayout gbl_editPanel = new GridBagLayout();
-        gbl_editPanel.columnWidths = new int[]{20, 210, 0, 20, 0};
+        gbl_editPanel.columnWidths = new int[]{20, 180, 0, 15, 0};
         gbl_editPanel.rowHeights = new int[]{20, 0, 0, 22, 0, 50, 15, 0, 22, 20, 0};
         gbl_editPanel.columnWeights = new double[]{2.0, 4.0, 0.0, 2.0, Double.MIN_VALUE};
         gbl_editPanel.rowWeights = new double[]{2.0, 0.0, 0.0, 0.0, 0.0, 4.0, 1.0, 0.0, 0.0, 2.0, Double.MIN_VALUE};
         editPanel.setLayout(gbl_editPanel);
         
         // 'Add' title
-        JLabel lblNewLabel_2 = new JLabel("Add a New Term");
-        lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 14));
-        GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-        gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
-        gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_2.gridx = 1;
-        gbc_lblNewLabel_2.gridy = 1;
-        editPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+        JLabel addTitle = new JLabel("Add a New Term");
+        addTitle.setFont(new Font("Tahoma", Font.BOLD, 14));
+        GridBagConstraints gbc_addTitle = new GridBagConstraints();
+        gbc_addTitle.gridwidth = 2;
+        gbc_addTitle.anchor = GridBagConstraints.WEST;
+        gbc_addTitle.insets = new Insets(0, 0, 5, 5);
+        gbc_addTitle.gridx = 1;
+        gbc_addTitle.gridy = 1;
+        editPanel.add(addTitle, gbc_addTitle);
         
         // Add word field label
-        JLabel lblNewLabel = new JLabel("Term");
-        lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
-        GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
-        gbc_lblNewLabel.gridx = 1;
-        gbc_lblNewLabel.gridy = 2;
-        editPanel.add(lblNewLabel, gbc_lblNewLabel);
+        JLabel addTermLabel = new JLabel("Term");
+        addTermLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
+        GridBagConstraints gbc_addTermLabel = new GridBagConstraints();
+        gbc_addTermLabel.gridwidth = 2;
+        gbc_addTermLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_addTermLabel.anchor = GridBagConstraints.WEST;
+        gbc_addTermLabel.gridx = 1;
+        gbc_addTermLabel.gridy = 2;
+        editPanel.add(addTermLabel, gbc_addTermLabel);
         
         // Add word field
         addField = new JTextField();
@@ -191,14 +197,14 @@ public class ClientView implements Runnable {
         editPanel.add(addField, gbc_addField);
         
         // Add definition field label
-        JLabel lblNewLabel_1 = new JLabel("Definition");
-        lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
-        GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-        gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTHWEST;
-        gbc_lblNewLabel_1.gridx = 1;
-        gbc_lblNewLabel_1.gridy = 4;
-        editPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+        JLabel addDefinitionLabel = new JLabel("Definition");
+        addDefinitionLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
+        GridBagConstraints gbc_addDefinitionLabel = new GridBagConstraints();
+        gbc_addDefinitionLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_addDefinitionLabel.anchor = GridBagConstraints.NORTHWEST;
+        gbc_addDefinitionLabel.gridx = 1;
+        gbc_addDefinitionLabel.gridy = 4;
+        editPanel.add(addDefinitionLabel, gbc_addDefinitionLabel);
         
         // Add definition field
         descriptionField = new JTextField();
@@ -227,14 +233,15 @@ public class ClientView implements Runnable {
         });
         
         // 'Remove' title
-        JLabel lblNewLabel_4 = new JLabel("Remove a Term");
-        lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 14));
-        GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
-        gbc_lblNewLabel_4.anchor = GridBagConstraints.WEST;
-        gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
-        gbc_lblNewLabel_4.gridx = 1;
-        gbc_lblNewLabel_4.gridy = 7;
-        editPanel.add(lblNewLabel_4, gbc_lblNewLabel_4);
+        JLabel removeTitle = new JLabel("Remove a Term");
+        removeTitle.setFont(new Font("Tahoma", Font.BOLD, 14));
+        GridBagConstraints gbc_removeTitle = new GridBagConstraints();
+        gbc_removeTitle.gridwidth = 2;
+        gbc_removeTitle.anchor = GridBagConstraints.WEST;
+        gbc_removeTitle.insets = new Insets(0, 0, 5, 5);
+        gbc_removeTitle.gridx = 1;
+        gbc_removeTitle.gridy = 7;
+        editPanel.add(removeTitle, gbc_removeTitle);
         
         // Remove word field
         removeField = new JTextField();
@@ -323,6 +330,13 @@ public class ClientView implements Runnable {
 	}
 	
 	/**
+	 * Initial showing in text area
+	 */
+	//private initialResults() {
+		
+	//}
+	
+	/**
 	 * Shows a response in the GUI's text pane
 	 */
 	public void showResults(List<Result> results) {
@@ -333,25 +347,32 @@ public class ClientView implements Runnable {
 		// Collect results together
 		int n = 1;
 		for (Result result : results) {
+			
+			// Separator between results
+	        if (n != 1) {
+	        	s.add("\n\n");
+		        styles.add("regular");
+	        }
+			
 			// title
 	        s.add("Definition " + n + "\n");
 	        styles.add("bold large");
 	        n++;
 	        
-	        // definition
-	        if (result.getDefinition().length() > 0) {
-	        	s.add(result.getDefinition() + "\n");
-	        	styles.add("regular");
+	        if (result.getPartOfSpeech().length() > 0) {
+	        	s.add(result.getPartOfSpeech() +"\n");
+	        	styles.add("italic");
 	        }
 	        
-	        if (result.getPartOfSpeech().length() > 0) {
-	        	s.add(result.getPartOfSpeech() + "\n");
-	        	styles.add("italic");
+	        // definition
+	        if (result.getDefinition().length() > 0) {
+	        	s.add(capitalise(result.getDefinition()) + ".\n");
+	        	styles.add("regular");
 	        }
 	        
 	        // Separator
 	        s.add("\n");
-	        styles.add("regular");
+	        styles.add("small");
 	        
 	        // Synonyms
 	        if (result.getSynonyms().size() > 0) {
@@ -363,7 +384,7 @@ public class ClientView implements Runnable {
 		    
 	        // Type of
 	        if (result.getTypeOf().size() > 0) {
-		        s.add("It is a type of: ");
+		        s.add("A type of: ");
 		        styles.add("bold");
 		        s.add(collectStrings(result.getTypeOf()));
 		        styles.add("regular");
@@ -371,7 +392,7 @@ public class ClientView implements Runnable {
 	        
 	        // Has Types
 	        if (result.getHasTypes().size() > 0) {
-		        s.add("It has the types: ");
+		        s.add("Has the types: ");
 		        styles.add("bold");
 		        s.add(collectStrings(result.getHasTypes()));
 		        styles.add("regular");
@@ -384,20 +405,23 @@ public class ClientView implements Runnable {
 		        s.add(collectStrings(result.getExamples()));
 		        styles.add("italic");
 	        }
-	        
-	        // Result separator
-	        s.add("\n\n\n");
-	        styles.add("regular");
 		}
 
-        textPane.setText("");
+		updateTextPane(s, styles);
+    }
+	
+	/**
+	 * Updates the search result text pane
+	 * Takes a list of strings to output, and each string's associated style
+	 */
+	private void updateTextPane(List<String> s, List<String> styles) {
+		textPane.setText("");
         MutableAttributeSet mas = textPane.getInputAttributes();
         mas.removeAttributes(mas);
         
         StyledDocument doc = textPane.getStyledDocument();
         addStylesToDocument(doc);
         
- 
         try {
             for (int i=0; i < s.size(); i++) {
                 doc.insertString(doc.getLength(), s.get(i),
@@ -406,21 +430,38 @@ public class ClientView implements Runnable {
         } catch (BadLocationException ble) {
             System.err.println("Couldn't insert initial text into text pane.");
         }
-    }
+	}
 	
+	/**
+	 * Concatenates an array of strings together into one string
+	 */
 	private String collectStrings(List<String> strings) {
 		
 		String collector = "";
         for (String s : strings) {
-        	collector = collector + s + ", ";
+        	collector += s + ", ";
         }
+        
+        // remove last comma
+        collector = collector.substring(0, collector.length() - 2);
+        
         return collector + "\n";
+	}
+	
+	/**
+	 * Capitalises a string
+	 */
+	private String capitalise(String str) {
+	    if(str == null || str.isEmpty()) {
+	        return str;
+	    }
+	    return str.substring(0, 1).toUpperCase() + str.substring(1);
 	}
 	
 	/**
 	 * Add some standard styles to the document
 	 */
-	protected void addStylesToDocument(StyledDocument doc) {
+	private void addStylesToDocument(StyledDocument doc) {
         //Initialize some styles.
         Style def = StyleContext.getDefaultStyleContext().
                         getStyle(StyleContext.DEFAULT_STYLE);
@@ -435,7 +476,7 @@ public class ClientView implements Runnable {
         StyleConstants.setBold(s, true);
  
         s = doc.addStyle("small", regular);
-        StyleConstants.setFontSize(s, 10);
+        StyleConstants.setFontSize(s, 6);
  
         s = doc.addStyle("large", regular);
         StyleConstants.setFontSize(s, 15);
